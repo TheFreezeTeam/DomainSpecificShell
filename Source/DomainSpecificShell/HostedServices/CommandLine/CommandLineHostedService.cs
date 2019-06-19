36999752338de7c06b05f1f8c9b0e5dfea8d851e
Steven T. Cramer <SteveTCramer@gmail.com>
2019-06-19 04:56:43 +0000

    Add CommandLineHostedService this is going to contain our request response loop
    move worker git can't tell its a move so its delete add .

---
+++ Source/DomainSpecificShell/HostedServices/CommandLine/CommandLineHostedService.cs
@@ -0,0 +1,25 @@
+﻿namespace DomainSpecificShell.HostedServices
+{
+  using Microsoft.Extensions.Hosting;
+  using Microsoft.Extensions.Logging;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  internal class CommandLineHostedService : BackgroundService
+  {
+    private readonly ILogger<CommandLineHostedService> Logger;
+    internal CommandLineHostedService(ILogger<CommandLineHostedService> aLogger)
+    {
+      Logger = aLogger;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken aCancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
