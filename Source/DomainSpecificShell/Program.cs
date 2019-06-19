bbc31d4281d26c07555e47696e5b5948cd62d18c
Steven T. Cramer <SteveTCramer@gmail.com>
2019-06-19 04:19:02 +0000

    Add Editor Config and cleanup accordingly

--- Source/DomainSpecificShell/Program.cs
+++ Source/DomainSpecificShell/Program.cs
@@ -1,24 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 namespace DomainSpecificShell
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.Extensions.Hosting;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                });
-    }
+  internal class Program
+  {
+    internal static IHostBuilder CreateHostBuilder(string[] aArgumentArray) =>
+    Host.CreateDefaultBuilder(aArgumentArray)
+      .ConfigureServices((aHostBuilderContext, aServiceCollection) =>
+        aServiceCollection.AddHostedService<Worker>());
+
+    internal static void Main(string[] aArgumentArray) =>
+      CreateHostBuilder(aArgumentArray).Build().Run();
+  }
 }
--- Source/DomainSpecificShell/Worker.cs
+++ Source/DomainSpecificShell/Worker.cs
@@ -1,29 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace DomainSpecificShell
 {
-    public class Worker : BackgroundService
+  using Microsoft.Extensions.Hosting;
+  using Microsoft.Extensions.Logging;
+  using System;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  public class Worker : BackgroundService
+  {
+    private readonly ILogger<Worker> Logger;
+
+    public Worker(ILogger<Worker> aLogger)
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
-        }
+      Logger = aLogger;
     }
+
+    protected override async Task ExecuteAsync(CancellationToken aCancellationToken)
+    {
+      while (!aCancellationToken.IsCancellationRequested)
+      {
+        Logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        await Task.Delay(1000, aCancellationToken);
+      }
+    }
+  }
 }
